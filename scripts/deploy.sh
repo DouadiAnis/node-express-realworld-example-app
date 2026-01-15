4a39f88a1b1ea383fb05822a65122d1c15c86f9a
Anis DOUADI <anis@MacBook-Air-de-Anis.local>
2026-01-15 12:28:10 +0100

    push

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
-set -e
+set -e # Arrête le script si une commande échoue
 
-echo "➡️  Début du déploiement..."
+echo "🚀 Démarrage du déploiement sur la VM..."
 
-echo "✅ Déploiement simulé réussi."
+# 1. Connexion au Docker Hub (pour avoir le droit de pull l'image privée)
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+# 2. Définition de l'image (Celle construite par le pipeline)
+export DOCKER_IMAGE="$DOCKER_USERNAME/student-ci-cd-project:latest"
+
+echo "⬇️  Téléchargement de la dernière version de l'image : $DOCKER_IMAGE"
+# C'est cette ligne qui force la mise à jour !
+docker compose -f docker-compose.yml pull api
+
+echo "♻️  Redémarrage des services..."
+# Le '-d' lance en fond, et Docker ne recrée QUE ce qui a changé (l'API)
+docker compose -f docker-compose.yml up -d
+
+echo "🧹 Nettoyage des vieilles images inutilisées..."
+docker image prune -f
+
+echo "✅ Déploiement terminé avec succès !"
